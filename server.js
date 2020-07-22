0c7b750a91feaf9e3c188c126b11c42a061a3828
aditi-mi <aditi@moderninfluence.com>
2020-07-22 00:42:17 -0500

    directory path changed

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/aditi-portfolio'));
+app.use(express.static(__dirname + '/dist/aditi-portfolio')); //giving the path to the express
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/portfolio/index.html'));
+        '/dist/aditi-portfolio/index.html'));
 });
 app.listen(process.env.PORT || 8080);
