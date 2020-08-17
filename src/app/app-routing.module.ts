56f019b3660cc6136f706edde331590925de3240
aditi-mi <aditi@moderninfluence.com>
2020-08-16 23:24:07 -0500

    basic layout setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', component: HomeComponent, data: { title: 'Home' } },
+{ path: 'about', component: AboutComponent, data: { title: 'About' } },
+{ path: 'projects', component: ProjectsComponent, data: { title: 'Projects' } }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
